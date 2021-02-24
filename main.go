b9c81d339192e1519c1145e7da86fa418383cbb1
James Musselwhite <james.musselwhite@gmail.com>
2021-02-24 13:17:41 -0800

    just mysql

--- main.go
+++ main.go
@@ -1,20 +1,47 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"log"
-	"net/http"
-)
+	"os"
+	"time"
 
+	_ "github.com/go-sql-driver/mysql"
+)
 
 func main() {
 	fmt.Println("App launched")
-	fs := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fs)
-	http.HandleFunc("/env", getEnv)
-	http.HandleFunc("/writeMysql", writeMysql)
-	http.HandleFunc("/readMysql", readMysql)
-	http.HandleFunc("/writeRedis", writeRedis)
-	http.HandleFunc("/readRedis", readRedis)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	hostname := os.Getenv("DEMO_APP_MYSQL_HOSTNAME")
+	database := os.Getenv("DEMO_APP_MYSQL_DATABASE")
+	username := os.Getenv("DEMO_APP_MYSQL_USERNAME")
+	password := os.Getenv("DEMO_APP_MYSQL_PASSWORD")
+	connectionString := fmt.Sprintf("%s:%s@tcp(%s:3306)/%s", username, password, hostname, database)
+	petname := "puppyface"
+	fmt.Println("connectionString: " + connectionString)
+	db, err := sql.Open("mysql", connectionString)
+	if err != nil {
+		fmt.Println("sql.Open error")
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	err = db.Ping()
+	if err != nil {
+		fmt.Println("db.Ping error")
+		panic(err.Error()) // proper error handling instead of panic in your app
+	}
+
+	_, err = db.Exec("CREATE TABLE IF NOT EXISTS pet ( id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, name VARCHAR(30) NOT NULL)")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		time.Sleep(2 * time.Second)
+		sql_statement := fmt.Sprintf("insert into pet (name) values ('%s')", petname)
+		_, err = db.Exec(sql_statement)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 }
